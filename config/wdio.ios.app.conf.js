e797259f0d62b3e63f4a033dd474637f8752f805
virunkumarn.si <virunkumarn.si@ocean.co.th>
2021-05-22 23:33:19 +0700

    test

--- config/wdio.ios.app.conf.js
+++ config/wdio.ios.app.conf.js
@@ -28,7 +28,7 @@ config.capabilities = [
         // https://github.com/appium/appium/releases/tag/v1.13.0
         'appium:automationName': 'XCUITest',
         // The path to the app
-        'appium:app': join(process.cwd(), './apps/iOS-Simulator-NativeDemoApp-0.2.1.app.zip'),
+        // 'appium:app': join(process.cwd(), './apps/iOS-Simulator-NativeDemoApp-0.2.1.app.zip'),
         // Read the reset strategies very well, they differ per platform, see
         // http://appium.io/docs/en/writing-running-appium/other/reset-strategies/
         'appium:noReset': true,
